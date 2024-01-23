fa2c675d3e015ec0f8a79eb044c5ff7e94f0ea1a
Askhad Arzybaev <aarzybaev@gmail.com>
2024-01-23 17:46:50 +0600

    applied all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
+import {Button, Message} from './types';
+import Appbar from './components/Appbar/Appbar';
+import Messages from './components/Messages/Messages';
+import Modal from './components/Modal/Modal';
+import Alert from './components/Alert/Alert';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(true);
+  const [messages, setMessages] = useState<Message[]>([
+    {id: 1, title: 'Title', message: 'My first message'},
+    {id: 2, title: 'Title', message: 'My second message'},
+    {id: 3, title: 'Title', message: 'My third message'}
+  ]);
+  const cancel = () => setShowModal(false);
+  const closeAlert = () => setShowAlert(false);
+
+  const modalButtons: Button[] = [
+    {type: 'primary', label: 'Continue', onClick: () => alert('clicked continue')},
+    {type: 'danger', label: 'Close', onClick: () => cancel()}
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+      <Appbar/>
+      </header>
+      <main className="container-fluid">
+        <div className="row mt-2">
+          <div className="col-4">
+            <h4>Messages</h4>
+            <Messages
+              messages={messages}
+              onClose={() => setShowModal(true)}
+            />
+
+            <Modal
+              show={showModal}
+              onClose={cancel}
+              title="Modal title"
+              buttons = {modalButtons}
+            >
+              <p className="p-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </Modal>
+
+            <Alert
+              show={showAlert}
+              type="warning"
+              onDismiss={closeAlert}
+            >This is a warning type alert</Alert>
+            <Alert
+              type="success">
+              This is a success type alert
+            </Alert>
+
+          </div>
+        </div>
+        <div className="col"></div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
